20e64fa8cd270e35a431ee6e26a2bc0e9139c689
Jashanpreet Singh <pjashan431@gmail.com>
2022-02-17 09:21:19 -0800

    add repl.it configuration file

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+ <head>
+   <title>PHP File!</title>
+   </head>
+<body>
+<h1>PHP File!</h1>
+
+<p>This is HTML!</p>
+
+<?php
+   echo "<p>This is PHP!</p>";
+?>
+<p>This is HTML!</p>
+
+</body>
+
+</html>
